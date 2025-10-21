64a9fafa70cf963b478f524e4ad2275f67cb09c0
Josep Monjo <jvmonjo@gmail.com>
2025-10-21 18:57:49 +0200

    chore: increment versió a 1.0.1

--- js/version.js
+++ js/version.js
@@ -4,5 +4,5 @@
  * entre el Service Worker, el PDF i el sistema d'actualitzacions
  */
 
-const APP_VERSION = '1.0.0';
-const CACHE_VERSION = 'v9';
+const APP_VERSION = '1.0.1';
+const CACHE_VERSION = 'v1-0-1';
